6e8f23203e66218890b2c91247bf19e5e73d7b40
ema <mccoy39082@163.com>
2025-01-02 16:20:00 +0800

    restruct: new common

--- Scripts/pack-zip.ps1
+++ Scripts/pack-zip.ps1
@@ -1,5 +1,5 @@
 Remove-Item ..\QuickLook.Plugin.DrawIOViewer.qlplugin -ErrorAction SilentlyContinue
 
-$files = Get-ChildItem -Path ..\QuickLook.Plugin\QuickLook.Plugin.DrawIOViewer\bin\Release\ -Exclude *.pdb,*.xml
+$files = Get-ChildItem -Path ..\Build\Release\ -Exclude *.pdb,*.xml
 Compress-Archive $files ..\QuickLook.Plugin.DrawIOViewer.zip
 Move-Item ..\QuickLook.Plugin.DrawIOViewer.zip ..\QuickLook.Plugin.DrawIOViewer.qlplugin
